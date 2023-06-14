d92becb090f7141e330c43f438cd486901b6c9b8
johan <lilljajohan@hotmail.com>
2023-06-14 16:31:14 +0200

    Chagne source

--- source/tic_matrix.m
+++
@@ -1,9 +0,0 @@
-function out = tic_matrix(tot_ion_current,NumScans)
-    tot_ion = zeros(size(tot_ion_current,1),max(NumScans));
-    for i = 1:size(tot_ion_current)
-        tot_ion(i,1:length(tot_ion_current{i,1})) = tot_ion_current{i,1};
-    end
-    tot_ion(tot_ion==0)=NaN;
-   out = cell(1,1);
-   out{1} = tot_ion;
-end
